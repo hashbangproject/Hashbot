57954c3363b8940ade9d9cbd622eeffb9ce8dc7b
Sultan Qasim Khan <sultanqasim@gmail.com>
2013-07-10 01:15:30 -0400

    Added basic wireless servo control program.

--- main.c
+++ main.c
@@ -1,111 +1,25 @@
-/*  Stellarino Demo Programs
-    Copyright (C) 2012-2013 Sultan Qasim Khan
-
-    This is part of Stellarino.
-
-    Stellarino is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Stellarino is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Stellarino.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include "stellarino.h"
 
-short r = 0, g = 85, b = 171, dr = 1, dg = 1, db = 1;
+int main() {
+	int a = 1500;
 
-void setup();
-void loop();
+	init();
+	pinMode(PD0, OUTPUT_SERVO);
 
-int main(void) {
-    init();
-    setup();
-    while(1) loop();
+	puts("Enabling UART 2.\n");
+	enableUART(2, 115200);
+
+	UARTputs(2, "Ready.\n");
+	puts("I told Amanda that I'm ready.\n");
+	while (1) {
+		puts("I'm waiting for a number.\n");
+		a = UARTgeti(2);
+		servoWrite(PD0, a);
+		puts("I got the number ");
+		puti(a);
+		putln();
+		UARTputs(2, "Moved to ");
+		UARTputi(2, a);
+		UARTputln(2);
+	}
 }
-
-// 2-Speed LED Colour Fade Demo
-void setup() {
-    pinMode(RED_LED, OUTPUT_PWM);
-    pinMode(GREEN_LED, OUTPUT_PWM);
-    pinMode(BLUE_LED, OUTPUT_PWM);
-    pinMode(SW1, INPUT_PULLUP);
-}
-
-void loop() {
-    analogWrite(RED_LED, r);
-    analogWrite(GREEN_LED, g);
-    analogWrite(BLUE_LED, b);
-
-    if (r >= 255) dr = -1;
-    else if (r <= 0) dr = 1;
-    r += dr;
-
-    if (g >= 255) dg = -1;
-    else if (g <= 0) dg = 1;
-    g += dg;
-
-    if (b >= 255) db = -1;
-    else if (b <= 0) db = 1;
-    b += db;
-
-    if (digitalRead(SW1)) delay(5);
-    else delayMicroseconds(1000);
-}
-
-/*
-// UART Adder Demo Code
-#include "stellarino.h"
-
-int main(void) {
-    init();
-    while(1) {
-        puts("Enter numbers:\n");
-        long a = geti(), b = geti();
-        puti(a);
-        puts(" + ");
-        puti(b);
-        puts(" = ");
-        puti(a + b);
-        putln();
-    }
-}
-*/
-
-/*
-// MCP4921 DAC Demo using SPI
-// Generates a sawtooth wave, with the DAC connected to SSI0 and the
-// LDAC pin on PD0. SPI communication at 16 MHz.
-#include "stellarino.h"
-
-void DACWrite(short val);
-
-int main(void) {
-    init();
-    pinMode(PD0, OUTPUT);
-    enableSPI(0, 16, 16000000);
-    short a = 0;
-    while (1) {
-        if (a > 4095) a = 0;
-        DACWrite(a);
-        a += 5;
-        delayMicroseconds(5);
-    }
-}
-
-void DACWrite(short val) {
-    if (val < 0) val = 0;
-    else if (val > 4095) val = 4095;
-    SPIWrite(0, val + bit16[12] + bit16[13]);
-
-    // Trigger output on DAC
-    digitalWrite(PD0, LOW);
-    digitalWrite(PD0, HIGH);
-}
-*/
